df6942bcfd56346b087f9535982d7153edbb0a59
Anselem <anselemodims@gmail.com>
2022-03-22 09:46:05 +0100

    Test: write tests for the show view

--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -1,16 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Users controller', :type => :request do
-  before :each do
-    get "/users"
-  end
   describe 'GET /index' do
+    before :each do
+      get "/users"
+    end
     it 'renders the right template' do
       expect(response).to render_template(:index)
     end
 
     it 'the response body includes the correct placeholder text' do
       expect(response.body).to include("Here you will find a list of all users")
+    end
+  end
+
+  describe 'GET /show' do
+    before :each do
+      get "/users/1"
+    end
+    it 'renders the right template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'the response body includes the correct placeholder text' do
+      expect(response.body).to include("You can get one user here")
     end
   end
 end
