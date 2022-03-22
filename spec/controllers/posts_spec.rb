9aa34f61e53dd1099ff9ed98194739af34add64f
Anselem <anselemodims@gmail.com>
2022-03-22 10:02:43 +0100

    Fix linters

--- spec/controllers/posts_spec.rb
+++ spec/controllers/posts_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'Posts controller', type: :request do
   describe 'GET /index' do
     before :each do
-      get "/users/1/posts"
+      get '/users/1/posts'
     end
     it 'renders the right template' do
       expect(response).to render_template(:index)
     end
 
     it 'the response body includes the correct placeholder text' do
-      expect(response.body).to include("Here are the post for all users")
+      expect(response.body).to include('Here are the post for all users')
     end
 
     it 'the correct response status' do
@@ -20,14 +20,14 @@ RSpec.describe 'Posts controller', type: :request do
 
   describe 'GET /show' do
     before :each do
-      get "/users/1/posts/2"
+      get '/users/1/posts/2'
     end
     it 'renders the right template' do
       expect(response).to render_template(:show)
     end
 
     it 'the response body includes the correct placeholder text' do
-      expect(response.body).to include("You will see only one post here")
+      expect(response.body).to include('You will see only one post here')
     end
 
     it 'the correct response status' do
