d8e2dcff71115b6bf89d0b79c7cedc12ab8041dd
Anselem <anselemodims@gmail.com>
2022-03-22 09:41:15 +0100

    Test: write test for checking the placeholder text

--- spec/controllers/users_spec.rb
+++ spec/controllers/users_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Users controller', :type => :request do
+  before :each do
+    get "/users"
+  end
   describe 'GET /index' do
     it 'renders the right template' do
-      get "/users"
-
       expect(response).to render_template(:index)
+    end
+
+    it 'the response body includes the correct placeholder text' do
+      expect(response.body).to include("Here you will find a list of all users")
     end
   end
 end
