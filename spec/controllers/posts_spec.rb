322e3548d6b85a65d9e16663560acd0f2d08be10
Anselem <anselemodims@gmail.com>
2022-03-22 09:51:17 +0100

    Test: write tests for the posts show routing and view

--- spec/controllers/posts_spec.rb
+++ spec/controllers/posts_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe 'Posts controller', :type => :request do
       expect(response.body).to include("Here are the post for all users")
     end
   end
+
+  describe 'GET /show' do
+    before :each do
+      get "/users/1/posts/2"
+    end
+    it 'renders the right template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'the response body includes the correct placeholder text' do
+      expect(response.body).to include("You will see only one post here")
+    end
+  end
 end
