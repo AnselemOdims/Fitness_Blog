cfc95949696b41d89247c4e4bc2be147cc415e94
Anselem <anselemodims@gmail.com>
2022-03-28 11:04:48 +0100

    Feat: create a method that updates the posts counter for a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, foreign_key: 'post_id'
+  has_many :likes, foreign_key: 'post_id'
+  after_save :update_posts_counter
+
+  private
+  def update_posts_counter
+    author.increment!(:posts_counter, by = 1)
+  end
 end
