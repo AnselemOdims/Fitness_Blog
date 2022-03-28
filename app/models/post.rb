b0c962100686590f1f948977b631534153051d13
Anselem <anselemodims@gmail.com>
2022-03-28 11:08:58 +0100

    Feat: create a method which returns the five most recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: 'post_id'
   after_save :update_posts_counter
 
+  def recent_comments
+    comments.order('created_at DESC').limit(5)
+  end
+
   private
   def update_posts_counter
     author.increment!(:posts_counter, by = 1)
