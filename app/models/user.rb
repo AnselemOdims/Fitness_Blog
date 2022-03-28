0715a1e023bf8a7d976e2cfae5e376bd06ffb1cd
Anselem <anselemodims@gmail.com>
2022-03-28 10:36:28 +0100

    Feat: create a method that returns three most recent posts for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
+
+  def get_recent_posts
+    posts.order('created_at DESC').limit(3)
+  end
 end
