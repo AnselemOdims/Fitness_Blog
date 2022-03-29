77d9176146819c08348952e370a04d1d7a307b0d
Anselem <anselemodims@gmail.com>
2022-03-29 21:49:45 +0100

    Feat: create the new and show methods for the Post controller class

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,22 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = @current_user.posts.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        flash[:success] = 'Post created successfully'
+        format.html { redirect_to @post }
+      else
+        flash.now[:error] = 'Ooops! Post could not be saved'
+        format.html { render :new }
+      end
+    end
+  end
 end
