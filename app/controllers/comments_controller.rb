bebda9d86513f575c5c5574074a9776cd823bc3f
Anselem <anselemodims@gmail.com>
2022-03-30 01:19:31 +0100

    Feat: create route for likes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def new
-    @post = Post.find_by(id: params[:post_id])
+    @post = Post.find(params[:post_id])
     @comment = Comment.new
   end
 
