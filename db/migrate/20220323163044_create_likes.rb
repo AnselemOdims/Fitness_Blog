c11b543bf16ecd00347caec7a67392b9a8285bfa
Anselem <anselemodims@gmail.com>
2022-03-30 10:57:14 +0100

    Bug: fix foreign key id issue

--- db/migrate/20220323163044_create_likes.rb
+++ db/migrate/20220323163044_create_likes.rb
@@ -3,6 +3,8 @@ class CreateLikes < ActiveRecord::Migration[7.0]
     create_table :likes do |t|
 
       t.timestamps
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
     end
   end
 end
