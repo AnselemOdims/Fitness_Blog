019d14f4e2980635aa35b55b2d1133b9d09f5a43
Anselem <anselemodims@gmail.com>
2022-03-31 09:01:30 +0100

    Feat: add validation for Name can't be blank to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  validates :name, presence: true
 
   def fetch_recent_posts
     posts.order('created_at DESC').limit(3)
