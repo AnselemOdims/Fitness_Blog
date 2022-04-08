511ccffe614d7e1870bfcb68883ee720a438bb58
Anselem <anselemodims@gmail.com>
2022-04-08 22:35:46 +0100

    Chore: fix typo

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -7,27 +7,27 @@ RSpec.describe 'Login', type: :feature do
     visit '/users/sign_in'
   end
 
-  it 'displays the username, password and submit inputs' do
+  it 'should display the username, password and submit inputs' do
     expect(page).to have_field('Username/Email')
     expect(page).to have_field('Password')
     expect(page).to have_button('Log in')
   end
 
-  it 'displays an error and does not sign the user in' do
+  it 'should display an error and does not sign the user in' do
     fill_in 'Username/Email', with: ''
     fill_in 'Password', with: ''
     click_button 'Log in'
     expect(page).to have_content('Invalid Username/Email or Password')
   end
 
-  it 'displays an error for invalid credentials' do
+  it 'should display an error for invalid credentials' do
     fill_in 'Username/Email', with: 'johndoe@gmail.com'
     fill_in 'Password', with: '123'
     click_button 'Log in'
     expect(page).to have_content('Invalid Username/Email or Password')
   end
 
-  it 'displays an error for valid credentials' do
+  it 'should route to the root path and display flash message for valid credentials' do
     fill_in 'Username/Email', with: 'johndoe@gmail.com'
     fill_in 'Password', with: '123abc'
     click_button 'Log in'
