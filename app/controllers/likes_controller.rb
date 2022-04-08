4d32a98854079af0521bb4c67d27f6aea94d5f15
Anselem <anselemodims@gmail.com>
2022-04-08 10:47:53 +0100

    Feat: authenticate user for every action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @post = Post.find(params[:post_id])
     @like = current_user.likes.new(post_id: @post.id)
